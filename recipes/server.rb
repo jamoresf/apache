4542e4245268176155bfa066e7d5210b908940bf
juanaito <juanito@calamidad.es>
2018-04-04 16:00:27 +0000

    cambios por cookstyle

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,9 +6,8 @@
 #
 package 'httpd'
 file '/var/www/html/index.html' do
-content '<h1>Hello, world!</h1>'
+  content '<h1>Hello, world!</h1>'
 end
 service 'httpd' do
-action [:enable, :start]
+  action [:enable, :start]
 end
-
