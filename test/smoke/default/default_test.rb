514a67a61f8e12aa2fec6ca1d5cdd225845bc7c3
juanito <juanaito@calamidad.es>
2018-04-05 16:27:17 +0000

    Add tests for default test suite

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,17 +5,16 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-  # This is an example test, replace with your own test.
-  describe user('root') do
-    it { should exist }
-  end
+# This is an example test, replace with your own test.
+describe user('root') do
+  it { should exist }
+end
 
 # This is an example test, replace it with your own test.
 describe port(80) do
   it { should be_listening }
 end
 
-
 describe command('curl localhost') do
-its('stdout') { should match('Hello, world') }
+  its('stdout') { should match('Hello, world') }
 end
